1b98af9c07e36a8a4359c16bfc785fec79716b50
Dan Webster <dan.r.webster@gmail.com>
2019-11-03 14:47:14 -0700

    Populate request service page, about page, update styling on top menu

--- src/app/pages/request-service-page/request-service-page.component.ts
+++ src/app/pages/request-service-page/request-service-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { CallService } from '@services/call.service';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-request-service-page',
@@ -9,10 +11,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class RequestServicePageComponent implements OnInit {
 
 	private _pageTitle: string;
+	private _subscription = new Subscription;
+	public callInProgress: boolean = false;
 
 	constructor(
 		private _router: Router,
-		private _route: ActivatedRoute
+		private _route: ActivatedRoute,
+		private _callService: CallService
 	) { }
 
 	ngOnInit() {
@@ -21,6 +26,12 @@ export class RequestServicePageComponent implements OnInit {
 
 	public get pageTitle(): string {
 		return this._pageTitle;
+	}
+
+	public callServer(type: string): void {
+		this._callService.callServer(type);
+		this.callInProgress = true;
+		this._subscription = timer(300000).subscribe(() => this.callInProgress = false);
 	}
 
 }
