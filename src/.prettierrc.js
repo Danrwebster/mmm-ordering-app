32b6441ece4fa2b643ea4906e902ea8a665dd741
Dan Webster <dan.r.webster@gmail.com>
2020-02-25 19:37:31 -0700

    Add dev api URLs for menu and item calls

---
+++ src/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	trailingComma: 'es5',
+	printWidth: 140,
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	jsxSingleQuote: true,
+	jsxBracketSameLine: true,
+	htmlWhitespaceSensitivity: 'css',
+};
--- src/app/shared/_services/menu.service.ts
+++ src/app/shared/_services/menu.service.ts
@@ -5,81 +5,73 @@ import { catchError, retry } from 'rxjs/operators';
 import { IMenu, ICategory, IMenuItemDetails, IModifierGroup } from '@models/menu.model';
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class MenuService {
-
-	private _url: string = 'assets/data/';
+	private _localUrl: string = 'assets/data/';
+	private _url: string = 'https://qy5ux66qpd.execute-api.us-west-2.amazonaws.com/dev';
 	private _menuItem: IMenuItemDetails = <IMenuItemDetails>{};
 	private _showModal: boolean = false;
 	public $menuItem: Subject<IMenuItemDetails> = new Subject<IMenuItemDetails>();
 	public $topMenu: BehaviorSubject<IMenu[]> = new BehaviorSubject<IMenu[]>([]);
 
-	constructor(
-		private _http: HttpClient
-	) { }
+	constructor(private _http: HttpClient) {}
 
-	public initialize() {
-	}
+	public initialize() {}
 
 	public getTopMenu(): Observable<HttpResponse<IMenu[]>> {
-		return this._http.get<IMenu[]>(this._url + 'menus/top-menu.json', { observe: 'response' })
-			.pipe(
-				retry(3),
-				catchError(this.handleError)
-			);
+		return this._http
+			.get<IMenu[]>(this._url + '/menus', { observe: 'response' })
+			.pipe(retry(3), catchError(this.handleError));
 	}
 
 	public getCategoryMenu(categoryId: string): Observable<HttpResponse<ICategory>> {
-		return this._http.get<ICategory>(this._url + 'menus/' + categoryId + '/category-list.json', { observe: 'response' })
-			.pipe(
-				retry(3),
-				catchError(this.handleError)
-			);
+		return this._http
+			.get<ICategory>(this._url + '/menus/' + categoryId, {
+				observe: 'response',
+			})
+			.pipe(retry(3), catchError(this.handleError));
 	}
 
 	public getMyMobileMenu(categoryId: string): Observable<HttpResponse<ICategory>> {
-		return this._http.get<ICategory>(this._url + 'menus/' + categoryId + '/category-list.json', { observe: 'response' })
-			.pipe(
-				retry(3),
-				catchError(this.handleError)
-			);
+		return this._http
+			.get<ICategory>(this._localUrl + '/menus/' + categoryId + '/category-list.json', { observe: 'response' })
+			.pipe(retry(3), catchError(this.handleError));
 	}
 
 	private getItemDetails(sku: string): Observable<HttpResponse<IMenuItemDetails>> {
-		return this._http.get<IMenuItemDetails>(this._url + 'items/' + sku + '.json', { observe: 'response' })
-			.pipe(
-				retry(3),
-				catchError(this.handleError)
-			);
+		return this._http
+			.get<IMenuItemDetails>(this._url + '/items/' + sku, {
+				observe: 'response',
+			})
+			.pipe(retry(3), catchError(this.handleError));
 	}
 
 	public lookupItem(sku: string) {
-		this.getItemDetails(sku)
-			.subscribe(response => {
-				this._menuItem.description = response.body.description;
-				this._menuItem.descriptor = response.body.descriptor;
-				this._menuItem.imageURL = response.body.imageURL ? response.body.imageURL : undefined;
-				if (response.body.modifierGroup) {
-					this._menuItem.modifierGroup = [];
-					response.body.modifierGroup.forEach(value => {
-						const modifierGroupItem = <IModifierGroup>{
-							'groupName': value.groupName,
-							'modifiers': value.modifiers,
-							'required': value.required,
-							'min': value.min ? value.min : (value.required ? 1 : 0),
-							'max': value.max ? value.max : undefined
-						};
-						this._menuItem.modifierGroup.push(modifierGroupItem);
-					});
-				} else {
-					this._menuItem.modifierGroup = [];
-				}
-				this._menuItem.myMenu = response.body.myMenu;
-				this._menuItem.options = response.body.options;
+		this.getItemDetails(sku).subscribe(response => {
+			this._menuItem.description = response.body.description;
+			this._menuItem.descriptor = response.body.descriptor;
+			this._menuItem.imageURL = response.body.imageURL ? response.body.imageURL : undefined;
+			if (response.body.modifierGroup) {
+				this._menuItem.modifierGroup = [];
+				response.body.modifierGroup.forEach(value => {
+					const modifierGroupItem = <IModifierGroup>{
+						groupName: value.groupName,
+						modifiers: value.modifiers,
+						required: value.required,
+						min: value.min ? value.min : value.required ? 1 : 0,
+						max: value.max ? value.max : undefined,
+					};
+					this._menuItem.modifierGroup.push(modifierGroupItem);
+				});
+			} else {
+				this._menuItem.modifierGroup = [];
+			}
+			this._menuItem.myMenu = response.body.myMenu;
+			this._menuItem.options = response.body.options;
 
-				this.$menuItem.next(this._menuItem);
-			});
+			this.$menuItem.next(this._menuItem);
+		});
 	}
 
 	public get menuItem(): IMenuItemDetails {
@@ -105,12 +97,9 @@ export class MenuService {
 		} else {
 			// The backend returned an unsuccessful response code.
 			// The response body may contain clues as to what went wrong,
-			console.error(
-				`Backend returned code ${error.status}, ` +
-				`body was: ${error.error}`);
+			console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
 		}
 		// return an observable with a user-facing error message
-		return throwError(
-			'Something bad happened; please try again later.');
+		return throwError('Something bad happened; please try again later.');
 	}
 }
